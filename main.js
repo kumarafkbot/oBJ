e32fe796cd28db4c1496ecc156b6ec8dac4b681b
kumarafkbot <96719832+kumarafkbot@users.noreply.github.com>
2023-03-16 19:42:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+status = ""
 function setup() {
     canvas=createCanvas(640,420)
     canvas.center()
-    
+    object_detector = ml5.objectDetector('cocossd' , modelLoaded)
+    document.getElementById("status").innerHTML = "Detecting Objects"
 }
+
+function modelLoaded() {
+    console.log("Model Is loaded");
+    status = true
+    objectDetector.detect(img , gotresults)
+
+}
+
+function gotresults(error , results) {
+    if (error) {
+        console.log(error)
+    } else {
+      console.log(results)  
+    }
+}
+
 
 function preload() {
     img = loadImage("dog_cat.jpg")
@@ -14,5 +32,12 @@ fill("red")
 text("dog" , 20 , 20)
 noFill()
 stroke("black")
-rect(20 , 20, 320 , 420 )
+rect(20 , 20, 320 , 390 )
+
+fill("red")
+text("cat" , 20 , 20)
+nofill()
+stroke(black)
+rect(360,230,360,230)
 }
+
